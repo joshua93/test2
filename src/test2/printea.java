efe8879e615695e7cba3c2ac07b6dbb975998d65
ICOM STUDENTS <4035a9@ece.uprm.edu>
2015-10-28 20:15:13 -0400

    Second Commit

--- src/test2/printea.java
+++ src/test2/printea.java
@@ -4,7 +4,7 @@ public class printea {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("lj;flkj;");
+		System.out.println("Esto es asi");
 	}
 
 }
