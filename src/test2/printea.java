fe817d5190cc88bd8f30e16de2f4448bbc364b0f
Joshua A. Bonilla <joshua.bonilla@upr.edu>
2015-10-28 20:20:35 -0400

    Siempre le anadi otro print

--- src/test2/printea.java
+++ src/test2/printea.java
@@ -5,6 +5,7 @@ public class printea {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esto es asi");
+		System.out.println("Blah loco");
 	}
 
 }
